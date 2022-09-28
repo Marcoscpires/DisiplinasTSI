677b78c4af87ebf4e46ef7d849db035c1effa251
Marcos <marcospires@alunos.utfpr.edu.br>
2022-09-27 22:32:58 -0300

    aspas

---
+++ src/exercicios/Ex7.java
@@ -0,0 +1,16 @@
+
+package exercicios;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Ex7 {
+    
+    public static void main(String[] args) {
+        List<Animal> animals = Arrays.asList(new Cat(), new Spider(), new Cat("Tekir"),
+                new Fish("Free Willy"), new Spider(), new Fish("Jaws"));
+        
+        
+        
+    }
+}
