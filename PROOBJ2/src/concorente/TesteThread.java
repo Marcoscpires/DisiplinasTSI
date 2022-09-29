c27205970be6d2c62cf60a9e7f59ac4a7c736691
Marcos <marcospires@alunos.utfpr.edu.br>
2022-09-28 22:28:23 -0300

    aspas6

---
+++ PROOBJ2/src/concorente/TesteThread.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concorente;
+
+/**
+ *
+ * @author aluno
+ */
+public class TesteThread {
+    
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Marcos Cordeiro Pires");
+        Thread.sleep(3000000);
+        System.out.println("Marcos Cordeiro Pires");
+    }
+    
+}
