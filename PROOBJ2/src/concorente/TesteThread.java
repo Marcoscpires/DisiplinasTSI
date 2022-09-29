921b694d2d95439c6bd36ee9b6b99b40e443bbd9
Marcos <marcospires@alunos.utfpr.edu.br>
2022-09-28 22:51:22 -0300

    aspas7

--- PROOBJ2/src/concorente/TesteThread.java
+++ PROOBJ2/src/concorente/TesteThread.java
@@ -9,12 +9,21 @@ package concorente;
  *
  * @author aluno
  */
-public class TesteThread {
+public class TesteThread extends Thread{
     
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException{
+//        System.out.println("Marcos Cordeiro Pires");
+//        Thread.sleep(300);
+//        System.out.println("Marcos Cordeiro Pires");
+ 
+          TesteThread thre = new TesteThread();
+
+          thre.start();
+    }
+
+    public void run(){
         System.out.println("Marcos Cordeiro Pires");
-        Thread.sleep(3000000);
-        System.out.println("Marcos Cordeiro Pires");
+
     }
     
 }
