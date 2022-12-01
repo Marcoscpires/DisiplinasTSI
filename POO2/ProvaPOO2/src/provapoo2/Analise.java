c9f7e69f0535c61d735fe9a0d7b4a9545f3a69ea
marcosUTFPR <marcosccpiress15@gmail.com>
2022-11-30 21:15:53 -0300

    netwonking poo2

--- POO2/ProvaPOO2/src/provapoo2/Analise.java
+++ POO2/ProvaPOO2/src/provapoo2/Analise.java
@@ -1,14 +1,17 @@
 package provapoo2;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class Analise {
     
     public static void main(String[] args) {
-        FileOutputStream fos = new FileInputStream("ColecaoCotacaoLFT22.txt");
-        ObjectInputStream oos = new ObjectInputStream(fos);
+        BufferedReader fos = new BufferedReader(new FileReader("ColecaoCotacaoLFT22.txt"));
+        ObjectInputStream oos = new ObjectInputStream(fos.readLine());
     }
     
     
